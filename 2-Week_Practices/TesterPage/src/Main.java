2c7415b3292d2fdfdf81fb98e953ec865a1ab9e2
deerborg <abrekfurkan@hotmail.com>
2024-02-24 19:22:46 +0300

    update a ArrayMaxMin.java

--- 2-Week_Practices/TesterPage/src/Main.java
+++ 2-Week_Practices/TesterPage/src/Main.java
@@ -1,10 +1,21 @@
-import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
-    
-        
+    public static void main(String[] args) {
+
+       int b;
+       int[] list = {100,150,5,30};
+       Arrays.sort(list);
+       b = Arrays.binarySearch(list, 5);
+       System.out.println(b);
+       String a = Arrays.toString(list);
+       
+       System.out.println(a);
+       
+       int[] c = Arrays.copyOf(list, 2);
+       System.out.println(Arrays.toString(c));
+       int[] d = Arrays.copyOfRange(list, 0, 3);
+        System.out.println(Arrays.toString(d));
+       
+       
+    }
 }
-       
-       
-
-       
-
--- 2-Week_Practices/TesterPage/src/Test.java
+++ 2-Week_Practices/TesterPage/src/Test.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Test {
     public static void main(String[] args) {
         int userValue;
         Scanner scanner = new Scanner(System.in);
