1f56933276daac2a37809861bb1a1ac4a595ab0c
deerborg <abrekfurkan@hotmail.com>
2024-02-25 16:14:33 +0300

    update

--- 2-Week_Practices/EvenNumberLoop/src/Main.java
+++ 2-Week_Practices/EvenNumberLoop/src/Main.java
@@ -6,16 +6,15 @@ public class Main {
         int[] list = {1,1,1,3,3,4,5,5,6,7,8,9,9,10,11,12,13,13,13,2,2,4,4,4,44,44,444,22,20,14,14};
         Arrays.sort(list);
         int sayac = 0;
-        System.out.print("Tekrar eden cift sayilar ---> ");
         for(int i = 0 ; i < list.length ; i++){
             for(int j = 0; j < list.length ; j++){
                 if(list[i] == list[j] && i != j){
-                    if(list[i] % 2 == 0 || list[j] % 2 == 0){
-                        System.out.print(+list[i] + " ");
-                        break;
-                    } 
+                    break;
                 }
+                
             }
+            
+            
         }
 
     }       
--- 2-Week_Practices/itsA/src/Main.java
+++ 2-Week_Practices/itsA/src/Main.java
@@ -1,23 +1,27 @@
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String[][] letter = new String[7][4];
-
-        for (int i = 0; i < letter.length; i++) {
-            for (int j = 0; j < letter[i].length; j++) {
-                if (i == 0 || i == 3 || i==6) {
-                    letter[i][j] = " * ";
-                } else if (j == 0 || j == 3) {
-                    letter[i][j] = " * ";
+        
+        String[][] list = new String[7][4];
+        for (int i = 0; i < list.length;i++) {
+            for (int j = 0; j < list[i].length; j++) {
+                if(i==0 || i == 3){
+                    list[i][j] = " * ";
                 }
-                else {
-                    letter[i][j] = "   ";
+                else if( j == 0 || j == 3){
+                    list[i][j] = " * ";
                 }
+                else if(i == 6){
+                    list[i][j] = " * ";
+                }
+                else{
+                    list[i][j] = "   ";
+                } 
             }
         }
-
-        for (String[] row : letter){
-            for (String col : row){
+        for (String[] row : list) {
+            for (String col : row) {
                 System.out.print(col);
             }
             System.out.println();
