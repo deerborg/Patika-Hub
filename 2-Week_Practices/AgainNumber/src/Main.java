74b7dded47a9446fcf52ae0f9d9b0daa271a567c
deerborg <abrekfurkan@hotmail.com>
2024-02-26 03:45:02 +0300

    update a againnumber

--- 2-Week_Practices/AgainNumber/src/Main.java
+++ 2-Week_Practices/AgainNumber/src/Main.java
@@ -2,43 +2,40 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       int ekle = 1;
-       int say = 0;
-       int tekrar = 0;
-       Scanner scanner = new Scanner(System.in);
-       System.out.print("Dizi boyutunu girin: ");
-       int diziBoyutu = scanner.nextInt();
-       int[] dizi = new int[diziBoyutu];
+      System.out.print("Dizinin boyutu  : ");
+        Scanner scanner = new Scanner(System.in);
+        int diziBoyutu = scanner.nextInt();
 
-       // dizi olusturma
-       for(int i = 0 ; i < dizi.length ; i++){
-           System.out.print(ekle + ". elemani giriniz: ");
-           dizi[i] = scanner.nextInt();
-           ekle++;
-       }
-       // dizi ciktisi
-        System.out.print("Olusturulan yeni dizi =====> ");
-        
-       for(int i = 0; i < dizi.length; i++){
-           System.out.print(dizi[i] + " ");
-       }
-       // tekrari sayan dongu
-       for(int i = 0 ; i < dizi.length; i++){
-           for(int j = 0; j < dizi.length; j++){
-               if(dizi[i] == dizi[j]){
-                   say++;
-                   break;
-               }
-               else{
-                   say = 0;
-                   say++;
-                   break;
-               }
-           }
-           System.out.println("");
-           
-           System.out.println(dizi[i] + " sayisi " + say +" kere tekrar etti");
-       }
+        int[] dizi = new int[diziBoyutu];
+        int[] dizi2 = new int[diziBoyutu];
+
+        for (int i = 0; i < dizi.length; i++) {
+            System.out.print((i + 1) + "- Elemani giriniz : ");
+            dizi[i] = scanner.nextInt();
+        }
+        Arrays.sort(dizi);
+        System.out.println("Dizi  : " + Arrays.toString(dizi));
+
+        int tekrar = 0;
+        for (int r : dizi) {
+            int say = 0;
+            for (int j = 0; j < dizi.length; j++) {
+                if (r == dizi2[j]) {
+                    say++;
+                }
+            }
+            if (say == 0) {
+                int say2 = 0;
+                for (int i : dizi) {
+                    if (r == i) {
+                        say2++;
+                    }
+                }
+                dizi2[tekrar] = r;
+                tekrar++;
+                System.out.println(r + " sayisindan " + say2 + " tane var ");
+            }
+        }
     }
 }             
         
